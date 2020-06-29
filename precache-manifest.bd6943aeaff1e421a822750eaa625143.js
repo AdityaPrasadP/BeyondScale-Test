3c75143c750b9f9339252a1ccc3ebd8e93d022c4
Innovapath <innovapath@sakeths-MacBook-Pro.local>
2020-06-29 16:46:17 -0700

    Updates

---
+++ precache-manifest.bd6943aeaff1e421a822750eaa625143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a66027d83916eff6d4889a516fac18",
+    "url": "/BeyondScale-Test.git/index.html"
+  },
+  {
+    "revision": "73c8bcfbc1cf09c9f3f9",
+    "url": "/BeyondScale-Test.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ede468d3a75fb179fa7c",
+    "url": "/BeyondScale-Test.git/static/js/2.af69c12d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BeyondScale-Test.git/static/js/2.af69c12d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c8bcfbc1cf09c9f3f9",
+    "url": "/BeyondScale-Test.git/static/js/main.e18fa281.chunk.js"
+  },
+  {
+    "revision": "553187e6b3fa9ef7763f",
+    "url": "/BeyondScale-Test.git/static/js/runtime-main.87ad56f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BeyondScale-Test.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
